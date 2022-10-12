2228861b9ee86b9cbeb19a8bfe2b3caa7cee7354
Ricardo Gonçalves <ricardo@mytiki.com>
2022-10-12 15:47:32 -0300

    Initial Commit

---
+++ tiki_sdk_ios/tiki_sdk_ios.h
@@ -0,0 +1,18 @@
+//
+//  tiki_sdk_ios.h
+//  tiki_sdk_ios
+//
+//  Created by Ricardo on 12/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for tiki_sdk_ios.
+FOUNDATION_EXPORT double tiki_sdk_iosVersionNumber;
+
+//! Project version string for tiki_sdk_ios.
+FOUNDATION_EXPORT const unsigned char tiki_sdk_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <tiki_sdk_ios/PublicHeader.h>
+
+
