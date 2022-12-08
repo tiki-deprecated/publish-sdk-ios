f664416f633ccb3fd95de24600b7f6a599d53b12
Ricardo Gonçalves <ricardo@mytiki.com>
2022-12-08 15:52:30 -0300

    add platforms

--- Package.swift
+++ Package.swift
@@ -5,7 +5,9 @@ import AppKit
 
 let package = Package(
     name: "TikiSdk",
-    description: "A Swift Package for adding TIKI's decentralized infrastructure to **iOS** projects. Add tokenized data ownership, consent, and rewards to your app in minutes.",
+    platforms: [
+            .iOS(.v14)
+    ],
     products: [
         .library(
             name: "TikiSdk",
@@ -24,7 +26,6 @@ let package = Package(
                            "sqlite3_flutter_libs",
                            "sqlite3",
                           ]),
-        
         .binaryTarget(name: "App",
                       url: "https://github.com/tiki/tiki-sdk-flutter/releases/download/0.0.17/App_release.xcframework.zip",
                       checksum: "7cddad9f12c44424a6f5bcebb5def5c6a63fac5f2a9f2de7760bbe913d43f154"),
