0aff5b5a21f63a092a8be597aeee7becb30c8611
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2023-03-22 17:33:07 -0300

    fix: TikiSdk.license method parameters

--- Tests/IntegrationTests/IntegrationTests.swift
+++ Tests/IntegrationTests/IntegrationTests.swift
@@ -8,16 +8,17 @@ import XCTest
 import SwiftUI
 
 class IntegrationTests: XCTestCase {
-
+    
     let origin = "com.mytiki.iostest"
     let publishingId = "e12f5b7b-6b48-4503-8b39-28e4995b5f88"
     let id = UUID().uuidString
-
+    
     
     func testInitSdk() async throws {
         do{
-            try await TikiSdk.config().initialize(publishingId: publishingId, id:id)
-            XCTAssert(TikiSdk.address != nil)
+            try TikiSdk.config().initialize(publishingId: publishingId, id:id, onComplete: {
+                XCTAssert(TikiSdk.address != nil)
+            })
         }catch{
             XCTFail(error.localizedDescription)
         }
@@ -25,67 +26,53 @@ class IntegrationTests: XCTestCase {
     
     func testTikiSdkConfig() async throws{
         do{
-            try await TikiSdk.config()
+            try TikiSdk.config()
                 .theme
-                    .primaryTextColor(.white)
-                    .primaryBackgroundColor(.white)
-                    .secondaryBackgroundColor(.white)
-                    .accentColor(.white)
-                    .fontFamily("test")
-                    .and()
+                .primaryTextColor(.white)
+                .primaryBackgroundColor(.white)
+                .secondaryBackgroundColor(.white)
+                .accentColor(.white)
+                .fontFamily("test")
+                .and()
                 .dark
-                    .primaryTextColor(.white)
-                    .primaryBackgroundColor(.white)
-                    .secondaryBackgroundColor(.white)
-                    .accentColor(.white)
-                    .fontFamily("test")
-                    .and()
+                .primaryTextColor(.white)
+                .primaryBackgroundColor(.white)
+                .secondaryBackgroundColor(.white)
+                .accentColor(.white)
+                .fontFamily("test")
+                .and()
                 .offer
-                    .id("randomId")
-                    .bullet(text: "test 1", isUsed: true)
-                    .bullet(text: "test 2", isUsed: false)
-                    .bullet(text: "test 3", isUsed: true)
-                    .ptr("source")
-                    .description("testing")
-                    .terms("myTerms")
-                    .use(usecases: [LicenseUsecase(LicenseUsecaseEnum.support)])
-                    .tag(TitleTag(TitleTagEnum.advertisingData))
-                    .duration(365 * 24 * 60 * 60)
-                    .permission(Permission.camera)
-                    .add()
+                .id("randomId")
+                .bullet(text: "test 1", isUsed: true)
+                .bullet(text: "test 2", isUsed: false)
+                .bullet(text: "test 3", isUsed: true)
+                .ptr("source")
+                .description("testing")
+                .use(usecases: [LicenseUsecase(LicenseUsecaseEnum.support)])
+                .tag(TitleTag(TitleTagEnum.advertisingData))
+                .duration(365 * 24 * 60 * 60)
+                .permission(Permission.camera)
+                .add()
                 .offer
-                    .id("randomId2")
-                    .bullet(text: "test 1", isUsed: true)
-                    .bullet(text: "test 2", isUsed: true)
-                    .bullet(text: "test 3", isUsed: true)
-                    .ptr("source2")
-                    .description("testing")
-                    .terms("myTerms")
-                    .use(usecases: [LicenseUsecase(LicenseUsecaseEnum.support)])
-                    .tag(TitleTag(TitleTagEnum.advertisingData))
-                    .duration(365 * 24 * 60 * 60)
-                    .permission(Permission.camera)
-                    .add()
+                .id("randomId2")
+                .bullet(text: "test 1", isUsed: true)
+                .bullet(text: "test 2", isUsed: true)
+                .bullet(text: "test 3", isUsed: true)
+                .ptr("source2")
+                .description("testing")
+                .use(usecases: [LicenseUsecase(LicenseUsecaseEnum.support)])
+                .tag(TitleTag(TitleTagEnum.advertisingData))
+                .duration(365 * 24 * 60 * 60)
+                .permission(Permission.camera)
+                .add()
                 .onAccept { _, __ in }
                 .onDecline { _, __ in }
                 .onSettings { }
                 .disableAcceptEnding(false)
                 .disableDeclineEnding(true)
-                .initialize(publishingId: publishingId, id:id)
-            XCTAssert(TikiSdk.address != nil)
-        }catch{
-            XCTFail(error.localizedDescription)
-        }
-    }
-
-    
-    func testInitSdkWithAddress() async throws {
-        do{
-            try await TikiSdk.config().initialize(publishingId: publishingId, id:id)
-            let address = TikiSdk.address
-            try await TikiSdk.config().initialize(publishingId: publishingId, id:id)
-            let address2 = TikiSdk.address
-            XCTAssertEqual(address, address2)
+                .initialize(publishingId: publishingId, id:id, onComplete: {
+                    XCTAssert(TikiSdk.address != nil)
+                })
         }catch{
             XCTFail(error.localizedDescription)
         }
@@ -93,31 +80,38 @@ class IntegrationTests: XCTestCase {
     
     func testLicense() async throws{
         do{
-            try await TikiSdk.config().initialize(publishingId: publishingId, id:id)
-            let offer = try Offer()
-                .id("randomId")
-                .bullet(text: "test 1", isUsed: true)
-                .bullet(text: "test 2", isUsed: false)
-                .bullet(text: "test 3", isUsed: true)
-                .ptr("source")
-                .description("testing")
-                .terms("path/terms.md")
-                .use(usecases: [LicenseUsecase(LicenseUsecaseEnum.support)])
-                .tag(TitleTag(TitleTagEnum.advertisingData))
-                .permission(Permission.camera)
-            let license = try await TikiSdk.license(offer: offer)
-            XCTAssertEqual(license.description, "testing")
-            XCTAssertEqual(license.uses[0].usecases[0].value, LicenseUsecase(LicenseUsecaseEnum.support).value)
-            XCTAssertEqual(license.title.tags[0].value, TitleTagEnum.advertisingData.rawValue)
+            try TikiSdk.config().initialize(publishingId: publishingId, id:id, onComplete: {
+                Task{
+                    do{
+                        let offer = try Offer()
+                            .id("randomId")
+                            .bullet(text: "test 1", isUsed: true)
+                            .bullet(text: "test 2", isUsed: false)
+                            .bullet(text: "test 3", isUsed: true)
+                            .ptr("source")
+                            .description("testing")
+                            .terms("terms")
+                            .use(usecases: [LicenseUsecase(LicenseUsecaseEnum.support)])
+                            .tag(TitleTag(TitleTagEnum.advertisingData))
+                            .permission(Permission.camera)
+                        let license = try await TikiSdk.license(
+                            offer.ptr!, offer.uses, offer.terms ?? "terms", tags: offer.tags, licenseDescription: offer.description, expiry: offer.expiry)
+                        XCTAssertEqual(license.description, "testing")
+                        XCTAssertEqual(license.uses[0].usecases[0].value, LicenseUsecase(LicenseUsecaseEnum.support).value)
+                        XCTAssertEqual(license.title.tags[0].value, TitleTagEnum.advertisingData.rawValue)
+                    }catch{
+                        XCTFail()
+                    }
+                }
+            })
         }catch{
-            print(error)
-            XCTFail(error.localizedDescription)
+            XCTFail()
         }
     }
     
     func testGuard() async throws{
         do{
-            try await TikiSdk.config().initialize(publishingId: publishingId, id:id)
+            try TikiSdk.config().initialize(publishingId: publishingId, id:id)
             let offer = try Offer()
                 .id("randomId")
                 .bullet(text: "test 1", isUsed: true)
@@ -125,11 +119,11 @@ class IntegrationTests: XCTestCase {
                 .bullet(text: "test 3", isUsed: true)
                 .ptr("source")
                 .description("testing")
-                .terms("path/terms.md")
+                .terms("terms")
                 .use(usecases: [LicenseUsecase(LicenseUsecaseEnum.support)])
                 .tag(TitleTag(TitleTagEnum.advertisingData))
                 .permission(Permission.camera)
-            let license = try await TikiSdk.license(offer: offer)
+            let _ = try await license(offer: offer)
             let guardResult = try await TikiSdk.guard(ptr: "source", usecases:[LicenseUsecase(LicenseUsecaseEnum.support)], destinations: [])
             XCTAssert(guardResult)
         }catch{
@@ -137,5 +131,8 @@ class IntegrationTests: XCTestCase {
             XCTFail(error.localizedDescription)
         }
     }
+}
 
+func license(offer: Offer) async throws -> LicenseRecord {
+    return try await TikiSdk.license( offer.ptr!, offer.uses, offer.terms!, tags: offer.tags, licenseDescription: offer.description,expiry: offer.expiry)
 }
