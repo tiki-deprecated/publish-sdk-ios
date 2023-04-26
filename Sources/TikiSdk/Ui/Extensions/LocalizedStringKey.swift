bb9dfebbb6671584115e5294cb26a0e45bb37624
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2023-04-26 11:57:57 -0300

    fix: temporary fix for example app async init

--- Sources/TikiSdk/Ui/Extensions/LocalizedStringKey.swift
+++ Sources/TikiSdk/Ui/Extensions/LocalizedStringKey.swift
@@ -1,8 +1,1 @@
-import Foundation
-import SwiftUI
 
-extension LocalizedStringKey {
-    var stringKey: String? {
-        Mirror(reflecting: self).children.first(where: { $0.label == "key" })?.value as? String
-    }
-}
