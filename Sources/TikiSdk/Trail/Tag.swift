e54a39a5af5cb977db689928af75aa0697bd31dd
Jessé Monteiro <jessemonteiroferreira@gmail.com>
2023-08-10 10:32:53 -0300

    Update Tag and TagCommon
    Updating the code to the new v3 code and sync with android files structure

--- Sources/TikiSdk/Trail/Tag.swift
+++ Sources/TikiSdk/Trail/Tag.swift
@@ -8,6 +8,28 @@
 
 import Foundation
 
-public class Tag : Codable{
-    let algo: String
+class Tag : Decodable , Encodable{
+    private let value: String
+    
+    private init(value: String) {
+        self.value = value
+    }
+    
+    convenience init(tag: TagCommon) {
+        self.init(value: tag.rawValue)
+    }
+    
+    static func custom(tag: String) -> Tag {
+        return Tag(value: "custom:\(tag)")
+    }
+    
+    static func from(tag: String) -> Tag {
+        if let common = TagCommon.from(value: tag) {
+            return Tag(value: common.rawValue)
+        } else if tag.starts(with: "custom:") {
+            return Tag(value: tag)
+        } else {
+            return custom(tag: tag)
+        }
+    }
 }
