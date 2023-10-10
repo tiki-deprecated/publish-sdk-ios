5719de8723fb8fbb1b304f0b1b12f816a2700c77
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2023-10-10 08:46:32 -0300

    fix: clean xcodebuild warnings

--- Sources/TikiSdk/Trail/rsp/RspLicense.swift
+++ Sources/TikiSdk/Trail/rsp/RspLicense.swift
@@ -15,7 +15,7 @@ public struct RspLicense: Rsp {
     public let requestId: String
     
     public init(from: [String : Any?]) {
-        self.requestId = RspLicense.nullToNil(value: from["requestId"]) != nil ?  from["requestId"] as! String : ""
+        self.requestId = RspLicense.nullToNil(value: from["requestId"] as? String ?? nil) != nil ?  from["requestId"] as! String : ""
         self.id = from["id"] as? String
         self.title = from["title"] != nil ? RspTitle(from: from["title"] as! [String: Any?]) : nil
         self.uses = (from["uses"] as! [[String: Any]]).map{ use in
@@ -23,6 +23,6 @@ public struct RspLicense: Rsp {
         } as [Use]
         self.terms = from["terms"] as? String
         self.description = from["description"] as? String
-        self.expiry = RspLicense.nullToNil(value: from["expiry"]) != nil ? Date(milliseconds: from["expiry"] as! Int64) : nil
+        self.expiry = RspLicense.nullToNil(value: from["expiry"] as? Int64) != nil ? Date(milliseconds: from["expiry"] as! Int64) : nil
     }
 }
