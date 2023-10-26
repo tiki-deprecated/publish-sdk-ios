5b83e3fd544aea711d6e0217abc50d9bb949a602
Jessé Monteiro <jessemonteiroferreira@gmail.com>
2023-10-25 23:58:49 -0300

    fix idp, payable and receipt documentation

--- TikiSdk/Idp/Idp.swift
+++ TikiSdk/Idp/Idp.swift
@@ -5,13 +5,25 @@
 
 import Foundation
 
+/// The `Idp` class handles interactions with the Identity Provider.
 public class Idp {
     private let channel: Channel
     
+    /// Initializes the `Idp` class with a given communication channel.
+    ///
+    /// - Parameter channel: The communication channel to the Identity Provider.
     public init(channel: Channel) {
         self.channel = channel
     }
     
+    /// Exports a key with the specified parameters.
+    ///
+    /// - Parameters:
+    ///   - keyId: The ID of the key to export.
+    ///   - isPublic: Indicates whether the key is public (default is false).
+    ///   - completion: A closure to be called upon completion with the export response.
+    /// - Throws: An error if the export operation fails.
+    /// - Returns: The export response, `RspExport`.
     public func export(keyId: String, isPublic: Bool = false, completion: @escaping (RspExport) -> Void) async throws -> RspExport{
         let request = ReqExport(keyId: keyId, isPublic: isPublic)
         return try await channel.request(method: IdpMethod.export, request: request) { rsp in
@@ -22,6 +34,16 @@ public class Idp {
         
     }
     
+    
+    /// Imports a key with the specified parameters.
+    ///
+    /// - Parameters:
+    ///   - keyId: The ID of the key to import.
+    ///   - key: The key data to import.
+    ///   - isPublic: Indicates whether the key is public (default is false).
+    ///   - completion: A closure to be called upon completion with the import response.
+    /// - Throws: An error if the import operation fails.
+    /// - Returns: The import response, `RspDefault`.
     public func importMethod(keyId: String, key: Data, isPublic: Bool = false, completion: @escaping (RspDefault) -> Void) async throws -> RspDefault {
         let request = ReqImport(keyId: keyId, key: key, isPublic: isPublic)
         return try await channel.request(method: IdpMethod.importMethod, request: request) { rsp in
@@ -31,6 +53,14 @@ public class Idp {
         }
     }
     
+    /// Generates or retrieves a key with the specified parameters.
+    ///
+    /// - Parameters:
+    ///   - keyId: The ID of the key to generate or retrieve.
+    ///   - overwrite: Indicates whether to overwrite an existing key (default is false).
+    ///   - completion: A closure to be called upon completion with the key response.
+    /// - Throws: An error if the key operation fails.
+    /// - Returns: The key response, `RspDefault`.
     public func key(keyId: String, overwrite: Bool = false, completion: @escaping (RspDefault) -> Void) async throws -> RspDefault{
         let request = ReqKey(keyId: keyId, overwrite: overwrite)
         return try await channel.request(method: IdpMethod.importMethod, request: request) { rsp in
@@ -40,6 +70,14 @@ public class Idp {
         }
     }
     
+    /// Signs a message with the specified key.
+    ///
+    /// - Parameters:
+    ///   - keyId: The ID of the key to use for signing.
+    ///   - message: The message data to sign.
+    ///   - completion: A closure to be called upon completion with the signature data.
+    /// - Throws: An error if the signing operation fails.
+    /// - Returns: The signature data as `Data`, or `nil` if there is an error.
     public func sign(keyId: String, message: Data, completion: (Data?) -> Void) async throws -> Data? {
         let request = ReqSign(keyId: keyId, message: message)
         let rspSign = try await channel.request(method: IdpMethod.sign, request: request) { rsp in
@@ -50,6 +88,15 @@ public class Idp {
         return signature
     }
     
+    /// Verifies a message's signature using the specified key.
+    ///
+    /// - Parameters:
+    ///   - keyId: The ID of the key to use for verification.
+    ///   - message: The message data to verify.
+    ///   - signature: The signature data to verify against.
+    ///   - completion: A closure to be called upon completion with the verification result.
+    /// - Throws: An error if the verification operation fails.
+    /// - Returns: `true` if the message is verified, `false` otherwise.
     public func verify(keyId: String, message: Data, signature: Data, completion: (Bool) -> Void) async throws -> Bool {
         let request = ReqVerify(keyId: keyId, message: message, signature: signature)
         let rspVerify = try await channel.request(method: IdpMethod.verify, request: request) { rsp in
@@ -60,6 +107,11 @@ public class Idp {
         return isVerified
     }
     
+    /// Retrieves a token from the Identity Provider.
+    ///
+    /// - Parameter completion: An optional closure to be called upon completion with the token.
+    /// - Throws: An error if token retrieval fails.
+    /// - Returns: The retrieved `Token`.
     public func token(completion: ((Token) -> Void)? = nil) async throws -> Token{
         let rspToken = try await channel.request(method: IdpMethod.token, request: ReqDefault()) { rsp in
             RspToken(from: rsp)
--- TikiSdk/Trail/Receipt.swift
+++ TikiSdk/Trail/Receipt.swift
@@ -5,13 +5,28 @@
 
 import Foundation
 
+/// The `Receipt` class handles interactions related to receipts on the blockchain.
 public class Receipt {
     private let channel: Channel
     
+    
+    /// Initializes the Receipt class with a given communication channel.
+    ///
+    /// - Parameter channel: The communication channel to the blockchain.
     init(channel: Channel) {
         self.channel = channel
     }
     
+    /// Creates a receipt with specified parameters.
+    ///
+    /// - Parameters:
+    ///   - payableId: The ID of the payable associated with the receipt.
+    ///   - amount: The amount of the receipt.
+    ///   - description: An optional description for the receipt.
+    ///   - reference: An optional reference for the receipt.
+    ///   - completion: An optional closure to be called upon completion with the receipt record.
+    /// - Throws: An error if receipt creation fails.
+    /// - Returns: The created `ReceiptRecord` if successful, or `nil` if there is an error.
     public func create(
         payableId: String,
         amount: String,
@@ -31,6 +46,13 @@ public class Receipt {
         return receipt
     }
     
+    /// Retrieves a receipt by its ID.
+    ///
+    /// - Parameters:
+    ///   - id: The ID of the receipt to retrieve.
+    ///   - completion: An optional closure to be called upon completion with the receipt record.
+    /// - Throws: An error if receipt retrieval fails.
+    /// - Returns: The retrieved `ReceiptRecord` if successful, or `nil` if there is an error.
     public func get(id: String, completion: ((ReceiptRecord?) -> Void)? = nil) async throws -> ReceiptRecord? {
         let receiptReq = ReqReceiptGet(id: id)
         let rspReceipt: RspReceipt = try await channel.request(
@@ -44,6 +66,13 @@ public class Receipt {
         return receipt
     }
     
+    /// Retrieves all receipts associated with a payable.
+    ///
+    /// - Parameters:
+    ///   - payableId: The ID of the payable to retrieve receipts for.
+    ///   - completion: An optional closure to be called upon completion with an array of receipt records.
+    /// - Throws: An error if receipt retrieval fails.
+    /// - Returns: An array of `ReceiptRecord` objects associated with the payable.
     public func all(payableId: String, completion: (([ReceiptRecord]) -> Void)? = nil) async throws -> [ReceiptRecord] {
         let receiptReq = ReqReceiptAll(payableId: payableId)
         let rspReceipts: RspReceipts = try await channel.request(
